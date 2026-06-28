062046126d9ec28c59aaf83e72b8317cdff290fc
Kay-Uwe Kirstein <kay-uwe@kirsteinhome.ch>
2026-06-28 16:31:47 +0200

    fix compiler warnings

--- tests/tests/tests-fibonacci_tests.adb
+++ tests/tests/tests-fibonacci_tests.adb
@@ -6,31 +6,38 @@ package body Tests.Fibonacci_Tests is
 
    package Fibonacci renames Tasks.Fibonacci;
 
+   overriding
    function Name (T : Fibonacci_Test) return AUnit.Message_String is
+      pragma Unreferenced (T);
    begin
       return Format ("Fibonacci Tests");
    end Name;
 
    procedure Test_Naive_0 (T : in out Test_Cases.Test_Case'Class) is
+      pragma Unreferenced (T);
    begin
       Assert (Fibonacci.Fib_Naive (0) = 0, "Fib_Naive (0) should be 0");
    end Test_Naive_0;
 
    procedure Test_Naive_1 (T : in out Test_Cases.Test_Case'Class) is
+      pragma Unreferenced (T);
    begin
       Assert (Fibonacci.Fib_Naive (1) = 1, "Fib_Naive (1) should be 1");
    end Test_Naive_1;
 
    procedure Test_Naive_5 (T : in out Test_Cases.Test_Case'Class) is
+      pragma Unreferenced (T);
    begin
       Assert (Fibonacci.Fib_Naive (5) = 5, "Fib_Naive (5) should be 5");
    end Test_Naive_5;
 
    procedure Test_Naive_10 (T : in out Test_Cases.Test_Case'Class) is
+      pragma Unreferenced (T);
    begin
       Assert (Fibonacci.Fib_Naive (10) = 55, "Fib_Naive (10) should be 55");
    end Test_Naive_10;
 
+   overriding
    procedure Register_Tests (T : in out Fibonacci_Test) is
       use AUnit.Test_Cases.Registration;
    begin
--- tests/tests/tests-fibonacci_tests.ads
+++ tests/tests/tests-fibonacci_tests.ads
@@ -4,13 +4,15 @@ with AUnit.Test_Cases;
 package Tests.Fibonacci_Tests is
    type Fibonacci_Test is new AUnit.Test_Cases.Test_Case with null record;
 
+   overriding
    procedure Register_Tests (T : in out Fibonacci_Test);
+   overriding
    function Name (T : Fibonacci_Test) return Message_String;
 
-   -- Optional fixture hooks:
-   --overriding
-   --procedure Set_Up (T : in out Test_Case);
-   --overriding
-   --procedure Tear_Down (T : in out Test_Case);
+   --  Optional fixture hooks:
+   --  overriding
+   --  procedure Set_Up (T : in out Test_Case);
+   --  overriding
+   --  procedure Tear_Down (T : in out Test_Case);
 
 end Tests.Fibonacci_Tests;
